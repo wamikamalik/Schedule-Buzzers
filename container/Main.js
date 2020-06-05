702d50e0038119ae639f7209332a0cc40536b08b
SPande1502 <55352209+SPande1502@users.noreply.github.com>
2020-06-06 00:38:53 +0800

    Update Main.js

--- container/Main.js
+++ container/Main.js
@@ -17,6 +17,7 @@ import {
  import home from './home'
  import assignments from './assignments'
  import schedule from './schedule'
+import about from './about'
 
 const DrawerNavigator = createDrawerNavigator();
 
@@ -45,6 +46,7 @@ class Main extends React.Component {
                     <DrawerNavigator.Screen name="Add Assignments" options={{drawerIcon: config => <Image style={styles.image1} source={require('../assets/assign.png')}/>}} component={assignments}/>
                     <DrawerNavigator.Screen name="Profile" options={{drawerIcon: config => <Image style={styles.image1} source={require('../assets/profile.png')}/>}} component={Profilepage}/>
                     <DrawerNavigator.Screen name="Account Details" options={{drawerIcon: config => <Image style={styles.image1} source={require('../assets/accnt.png')}/>}} component={accntDetails}/>
+                     <DrawerNavigator.Screen name="About the app" options={{drawerIcon: config => <Image style={styles.image1} source={require('../assets/abouticon.png')}/>}} component={about}/>                                                                                                                     
                     <DrawerNavigator.Screen name="Sign Out" options={{drawerIcon: config => <Image style={styles.image1} source={require('../assets/logout.png')}/>}} component={App}/>
                 </DrawerNavigator.Navigator>
             </NavigationContainer>
