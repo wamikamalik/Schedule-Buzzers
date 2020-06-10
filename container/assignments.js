fadbaf8b7b4bfc3c2ee4b3b9a9bbfa47ca0ea7f5
wamikamalik <wamikamalik@hotmail.com>
2020-06-10 13:29:35 +0400

    notes, update feature added, formatting to be done

--- container/assignments.js
+++ container/assignments.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { KeyboardAvoidingView, ImageBackground, Image, TextInput, Text, Button, ActivityIndicator, StyleSheet, Dimensions } from 'react-native'
+import { KeyboardAvoidingView, ScrollView, ImageBackground, Image, TextInput, Text, Button, ActivityIndicator, StyleSheet, Dimensions } from 'react-native'
 import { TouchableOpacity} from 'react-native-gesture-handler';
 import DatePicker from 'react-native-datepicker'
 import moment from 'moment'
@@ -14,7 +14,8 @@ import GreenButton from '../component/GreenButton';
          name: null,
          deadline: null,
          valid: false,
-         done: false
+         done: false,
+         notes: null
      }
 
      date = () => {
@@ -31,6 +32,8 @@ import GreenButton from '../component/GreenButton';
      handleUpdatename = name => this.setState({name})
 
      handleUpdatedeadline = deadline => this.setState({deadline})
+
+     handleUpdatenotes = notes => this.setState({notes})
 
      UpdateUser = () => {
         if(this.state.name==''||this.state.name==null){
@@ -49,39 +52,67 @@ import GreenButton from '../component/GreenButton';
                 .doc(user)
                 .collection('assignments')
                 .doc(this.state.name)
-                .set(
-                {
-                Module: this.state.mod,
-                Name: this.state.name,
-                Deadline: this.state.deadline
-                })
-                .then(() => {
+                .get()
+                .then((doc)=>{
+                    if(doc.exists) {
+                        doc.ref.update ({
+                            Module: this.state.mod,
+                            Name: this.state.name,
+                            Deadline: this.state.deadline,
+                            Notes: this.state.notes 
+                    })
                     this.setState({
                         mod: '',
                         name: '',
                         deadline: '',
                         isValid:false,
-                        done: false
-                        }) 
-                        alert("Assignment Added!!")
+                        done: false,
+                        notes: ''
                     })
-                }
+                    alert("Assignment Updated!!")
+                    }
+                    else {
+                        firebaseDb.firestore()
+                        .collection('users')
+                        .doc(user)
+                        .collection('assignments')
+                        .doc(this.state.name) 
+                        .set(
+                            {
+                            Module: this.state.mod,
+                            Name: this.state.name,
+                            Deadline: this.state.deadline,
+                            Notes: this.state.notes
+                            })
+                            .then(() => {
+                                this.setState({
+                                    mod: '',
+                                    name: '',
+                                    deadline: '',
+                                    isValid:false,
+                                    done: false,
+                                    notes: ''
+                                }) 
+                                    alert("Assignment Added!!")
+                            })
+                    }
+                })
+                .catch(function(error) {
+                    console.error("Error: ", error);
+                });
+            }
             }
             else if(date<today){
                 alert("Date has passed already!!")
                 this.setState({
-                    mod: '',
-                    name: '',
                     deadline: '',
                     isValid:false,
-                    done: false
+                    done: false,
                     }) 
             }
             else {
                 alert("Invalid Date!!")
                 this.setState({
-                    mod: '',
-                    name: '',
                     deadline: '',
                     isValid:false,
                     done: false
@@ -122,16 +153,96 @@ import GreenButton from '../component/GreenButton';
         }   
      }
 
+     HandleSearch = () => {
+        const user = firebaseDb.auth().currentUser.uid;
+            if (user) {
+                firebaseDb.firestore()
+                .collection('users')
+                .doc(user)
+                .collection('assignments')
+                .doc(this.state.name)
+                .get()
+                .then((doc)=>{
+                    if(!doc.exists) {
+                        alert("No such Assignment!")
+                    }
+                    else{
+                        firebaseDb.firestore()
+                        .collection('users')
+                        .doc(user)
+                        .collection('assignments')
+                        .doc(this.state.name)
+                        .get()
+                        .then((doc) => {
+                            if(doc.exists) {
+                                this.setState ({
+                                    mod: doc.data().Module,
+                                    deadline: doc.data().Deadline,
+                                    notes: doc.data().Notes
+                                })
+                            }
+                        })
+                        .catch(function(error) {
+                            console.error("Error finding document: ", error);
+                        });
+                    }
+                })
+            
+            } 
+    }
+
+    //  HandleUpdate = () => {
+    //     const user = firebaseDb.auth().currentUser.uid;
+    //     if (user) {
+    //         firebaseDb.firestore()
+    //         .collection('users')
+    //         .doc(user)
+    //         .collection('assignments')
+    //         .doc(this.state.name)
+    //         .get()
+    //         .then((doc)=>{
+    //             if(!doc.exists) {
+    //                 alert("No such Assignment!")
+    //             }
+    //             else{
+    //                 firebaseDb.firestore()
+    //                 .collection('users')
+    //                 .doc(user)
+    //                 .collection('assignments')
+    //                 .doc(this.state.name)
+    //                 .get()
+    //                 .then((doc) => {
+    //                     if(doc.exists) {
+    //                         doc.ref.update ({
+    //                             Module: this.state.mod,
+    //                             Name: this.state.name,
+    //                             Deadline: this.state.deadline,
+    //                             Notes: this.state.notes 
+    //                     })
+    //                     alert("Assignment Updated!!")
+    //                 }
+    //                 })
+    //                 .catch(function(error) {
+    //                     console.error("Error updating document: ", error);
+    //                 });
+    //             }
+    //         })
+        
+    //     } 
+    //  }
+
      render() {
          return (
              <KeyboardAvoidingView style={styles.container}>
+                 <ScrollView>
                 <ImageBackground style={{flex: 1, resizeMode: "cover"}} source={require('../assets/assignback.png')}>
                 <TouchableOpacity style={{marginTop: 20}} onPress={()=>this.props.navigation.openDrawer()}><Image style={styles.image} source={require('../assets/slideinw.png')}/>
                 </TouchableOpacity>
                 <Text style={styles.text}>Add Assignments</Text>
-                <Text style={styles.texta}>Input only Name to remove an Assignment.</Text>
+                <Text style={styles.texta}>Input only Name to remove an Assignment. To update input name and press on search.</Text>
                 <Text style={styles.texta}>Module</Text><TextInput style={styles.textInput} placeholder='Module Name' placeholderTextColor="white" onChangeText={this.handleUpdateMod} value={this.state.mod}></TextInput>
                 <Text style={styles.texta}>Name</Text><TextInput style={styles.textInput} placeholder='Assignment name' placeholderTextColor="white" onChangeText={this.handleUpdatename} value={this.state.name}></TextInput>
+                <GreenButton style={styles.button} onPress= {this.HandleSearch}>Search</GreenButton>
                 <Text style={styles.texta}>Deadline</Text>
                     <DatePicker
                     style={{width: 200, marginTop: 10, alignSelf:'center'}}
@@ -160,10 +271,13 @@ import GreenButton from '../component/GreenButton';
                     }}
                     onDateChange={this.handleUpdatedeadline}/> 
                     <Text style={styles.texta}>If calender is inaccessible, Please enter date below</Text><TextInput style={styles.textInput} placeholder="DD-MM-YYYY" placeholderTextColor="white" onChangeText={this.handleUpdatedeadline} value={this.state.deadline}></TextInput>
+                    <Text style={styles.texta}>Notes</Text><TextInput style={styles.textInputa} placeholder='Additional notes' placeholderTextColor="white" onChangeText={this.handleUpdatenotes} value={this.state.notes}></TextInput>
 
-                <GreenButton style={styles.button} onPress= {this.UpdateUser}>Add</GreenButton>
+                <GreenButton style={styles.button} onPress= {this.UpdateUser}>Add/Update</GreenButton>
+                {/* <GreenButton style={styles.button} onPress= {this.HandleUpdate}>Update</GreenButton> */}
                 <GreenButton style={styles.button} onPress= {this.HandleRemove}>Remove</GreenButton>
                 </ImageBackground>
+                </ScrollView>
              </KeyboardAvoidingView>
          )
      }
@@ -186,7 +300,7 @@ import GreenButton from '../component/GreenButton';
         fontWeight:'bold',
         fontSize: 28,
         alignSelf:'center',
-        marginTop: 30,
+        marginTop: 10,
         textDecorationLine: "underline",
         color: "white"
     },
@@ -201,6 +315,20 @@ import GreenButton from '../component/GreenButton';
         fontWeight: "bold",
         alignSelf:'center',
         alignItems: 'center',
+        color: 'white'
+      },
+      textInputa: {
+        //borderRadius:5,
+        //backgroundColor:'white',
+        fontSize: 20,
+        marginTop: 10,
+        marginLeft: 5,
+        width: 400,
+        height: 200,
+        fontWeight: "bold",
+        alignSelf:'center',
+        //alignItems: 'center',
+        paddingTop:5,
         color: 'white'
       },
       texta: {
