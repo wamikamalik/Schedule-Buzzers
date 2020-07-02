c68b463100aa4256234e64a41ae54f1ac9ead50a
wamikamalik <wamikamalik@hotmail.com>
2020-07-02 17:47:44 +0400

    uploaded food places data to firestore, code in upload

---
+++ upload/index.js
@@ -0,0 +1,23 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+const data = require("./data.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://schedule-buzzers-2.firebaseio.com"
+});
+
+const firestore = admin.firestore();
+const settings = {timestampsInSnapshots: true};
+firestore.settings(settings);
+if (data && (typeof data === "object")) {
+Object.keys(data).forEach(Name => {
+ firestore.collection("foodplaces").doc("locations").collection("Bukit Timah Campus").doc(Name).set(data[Name]).then((res) => {
+    console.log("Document " + Name + " successfully written!");
+}).catch((error) => {
+   console.error("Error writing document: ", error);
+});
+});
+}
